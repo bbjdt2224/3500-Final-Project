a48e2e817f88912b6ac19b82d30281dff01a0db2
Justin True <justin.d.true@wmich.edu>
2017-11-13 20:38:52 -0500

    new database query

--- app/Http/Controllers/ArtController.php
+++ app/Http/Controllers/ArtController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Art;
+use App\Likes;
 
 class ArtController extends Controller
 {
     public function gallery($catagory){
-
+    	$art = DB::raw("Select a.photo, l.likes, l.dislikes From Art a, Likes l Where a.aid = l.aid and catagory = '".$catagory."'");
+    	dd($art);
     	return view('images.viewCatagory()', compact('art'));
     }
 }
