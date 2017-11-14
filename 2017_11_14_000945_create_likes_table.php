81b1daf6709e4b8faa16f918217a32644f84feb9
Erik Larson <erik.a.larson@wmich.edu>
2017-11-13 19:23:26 -0500

    Add files via upload
    Changes to create tables

---
+++ 2017_11_14_000945_create_likes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->increments('uid');
+            $table->increments('aid');
+            $table->Boolean('Likes')
+            $table->Boolean('Dislikes')
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+}
