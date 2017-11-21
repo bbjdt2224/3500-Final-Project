67a30bf209c7788e3d784c75bced5a97e11c6bad
Erik Larson <erik.a.larson@wmich.edu>
2017-11-21 16:06:28 -0500

    Added insert function to ArtController File

--- app/Http/Controllers/ArtController.php
+++ app/Http/Controllers/ArtController.php
@@ -12,9 +12,10 @@ class ArtController extends Controller
     public function gallery($catagory){
     	$art = DB::select(DB::raw("Select a.photo, l.likes, l.dislikes From Arts a, Likes l Where a.aid = l.aid and catagory = '".$catagory."'"));
     	return view('images.viewCatagory()', compact('art'));
+    	$insertArt = DB::insert(DB::raw("Insert into Art VAULES (".$id.",".$Title.",".$Photo.",".$Square.",".$Catagory.","")")
     }
 
     public function add(){
-    	DB::select(DB::raw("  insert into table  "));
+    	DB::select(DB::raw("insert into table values ()"));
     }
 }
